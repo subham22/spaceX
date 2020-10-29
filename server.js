c715f72d9787eea7f06554e2abadbefdf05eef28
Kumar <52093611@acsind.com>
2020-10-29 13:48:28 +0530

    updated home page template

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+app.use(forceSSL());
 app.use(express.static(__dirname + '/dist/SpaceX'));
 
 app.get('/*', function(req,res) {
